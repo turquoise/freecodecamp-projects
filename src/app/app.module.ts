d63a4f67eb5a97d77544849d952f2a8f5c57b813
turquoise <monica.gosschalk@btinternet.com>
2017-08-26 14:35:30 +0100

    added material sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AmexioWidgetModule, CommonHttpService } from 'amexio-ng-extensions';
+import { CovalentLayoutModule, CovalentStepsModule  } from '@covalent/core';
+// (optional) Additional Covalent Modules imports
+import { CovalentHttpModule } from '@covalent/http';
+import { CovalentHighlightModule } from '@covalent/highlight';
+import { CovalentMarkdownModule } from '@covalent/markdown';
+import { CovalentDynamicFormsModule } from '@covalent/dynamic-forms';
+
+import {FlexLayoutModule} from "@angular/flex-layout";
+
+import {
+  MaterialModule,
+  MdMenuModule,
+  MdSidenavModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AmexioWidgetModule
+    BrowserAnimationsModule,
+    CovalentLayoutModule,
+    CovalentStepsModule,
+    CovalentHttpModule.forRoot(),
+    CovalentHighlightModule,
+    CovalentMarkdownModule,
+    CovalentDynamicFormsModule,
+    MaterialModule,
+    MdMenuModule,
+    MdSidenavModule,
+    FlexLayoutModule
+
   ],
-  providers: [CommonHttpService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
